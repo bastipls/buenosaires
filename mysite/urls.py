29df78128e45e85c997f464bc52bd83c038c3a91
Bastian Perez <bas.perezp@alumnos.duoc.cl>
2019-06-10 17:10:33 -0400

    asd

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
 from rest_framework.urlpatterns import format_suffix_patterns
+
 from buenosaires import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apiproveedores/',views.stock_proveedores.as_view()),
