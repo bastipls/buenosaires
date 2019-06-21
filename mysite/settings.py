948bec9f78e683ba5b1e51e231a3d384f76dc56d
Bastian Perez <bas.perezp@alumnos.duoc.cl>
2019-06-21 02:17:32 -0400

    asd

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,65 +123,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# asdasdasdasd
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 ROLEPERMISSIONS_MODULE = 'mysite.roles'
